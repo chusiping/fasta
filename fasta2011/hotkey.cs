f06bc6425ee829daf668717820672903ef8d6e62
chusiping <chusiping@qq.com>
2015-10-10 18:32:17 +0800

    none

--- fasta2011/hotkey.cs
+++ fasta2011/hotkey.cs
@@ -9,7 +9,7 @@ namespace fasta2011
     public class HotKey   
     {   
         //如果函数执行成功，返回值不为0。   
-        //如果函数执行失败，返回值为0。要得到扩展错误信息，调用GetLastError。   
+        //如果函数执行失败，返回值为0。要得到扩展错误信息，调用GetLastError。  2015-10-10 
         [DllImport("user32.dll", SetLastError = true)]   
         public static extern bool RegisterHotKey(   
             IntPtr hWnd,                //要定义热键的窗口的句柄   
